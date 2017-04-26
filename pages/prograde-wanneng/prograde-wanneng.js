6d3a917bf126f8349d6d70b8eeb46d32934d5699
Ted Wu <iswujixiang@gmail.com>
2017-04-27 01:17:05 +0800

    feat: 万能型

--- pages/prograde-wanneng/prograde-wanneng.js
+++ pages/prograde-wanneng/prograde-wanneng.js
@@ -1,19 +1,36 @@
 // pages/prograde-wanneng/prograde-wanneng.js
+const request           = require('../../utils/request');
+
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
-})
+    data:{
+    },
+    onLoad:function({pid}){
+        request.withSessionKey({
+            url: 'https://wenme.cc/terms/product_module_evaluation',
+            data: {
+                pid,
+                module_code: 6,
+                product_structure: '万能型'
+            }
+        })
+            .then(({
+                data: {
+                    err_code,
+                    evaluation_info
+                }
+            }) => {
+                if (err_code === 0) {
+                    [
+                        'avg_credit_rate_3y',
+                        'avg_surrender_charge_f5y',
+                        'credit_rate',
+                        'initial_fee',
+                        'last_year_credit_rate',
+                        'min_credit_rate',
+                        'volatile_credit_rate_3y'
+                    ].forEach(key => evaluation_info[key] = (evaluation_info[key]*100).toFixed(1));
+                    this.setData(evaluation_info);
+                }
+            });
+    }
+});
