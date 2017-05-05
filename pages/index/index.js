98598e9ded9f229357c9cc6b09eed0afa4e142c7
Ted Wu <iswujixiang@gmail.com>
2017-05-06 02:20:30 +0800

    feat: 产品列表页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,36 +1,48 @@
 //index.js
 //获取应用实例
 const app = getApp();
+
+let keyword;
 Page({
-  data: {
-    imgUrls: [
-      '../../images/home-tips01.jpg',
-      '../../images/home-tips02.jpg',
-      '../../images/home-tips03.jpg'
-    ],
-    autoplay: false,
-    interval: 5000,
-    duration: 1000,
+    data: {
+        imgUrls: [
+            '../../images/home-tips01.jpg',
+            '../../images/home-tips02.jpg',
+            '../../images/home-tips03.jpg'
+        ],
+        autoplay: false,
+        interval: 5000,
+        duration: 1000,
 
-    // tabbar  : app.globalData.tabbar
-  },
-  bindViewTap: function() {
-    wx.redirectTo({
-      url: '../catalogue/catalogue'
-    })
-  },
-  //事件处理函数
-  searchProduct() {
-
-  },
-  
-  scanQRCode() {
-    wx.scanCode({
-      success({result, scanType, charSet}) {
+        // tabbar  : app.globalData.tabbar
+    },
+    bindViewTap: function () {
+        wx.redirectTo({
+            url: '../catalogue/catalogue'
+        })
+    },
+    bindKeyword({detail: {value}}) {
+        keyword = value;
+    },
+    searchProduct() {
+        // if (!keyword) {
+        //     return wx.showToast({
+        //         title: '请输入关键字',
+        //         duration: 1000
+        //     });
+        // }
         wx.navigateTo({
-          url: `../product-detail/product-detail?product_iachina_link=${encodeURIComponent(result).replace(/\./g, "%2E")}`
+            url: `../product-list/product-list?keyword=${keyword}`
         });
-      }
-    })
-  }
+    },
+
+    scanQRCode() {
+        wx.scanCode({
+            success({result}) {
+                wx.navigateTo({
+                    url: `../product-detail/product-detail?product_iachina_link=${encodeURIComponent(result).replace(/\./g, "%2E")}`
+                });
+            }
+        })
+    }
 });
--- pages/product-list/product-list.js
+++ pages/product-list/product-list.js
@@ -1,19 +1,26 @@
 // pages/product-list/product-list.js
+const request           = require('../../utils/request');
+
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
-})
+    data:{},
+    toDetail({currentTarget}) {
+        wx.redirectTo({
+            url: `../product-detail/product-detail?pid=${currentTarget.dataset.pid}`
+        });
+    },
+    onLoad:function(data){
+        request.withSessionKey({
+            url: 'https://wenme.cc/terms/terms_search',
+            data
+        })
+            .then(({
+                data: {
+                    product_list,
+                    total_page_count,
+                    total_product_count
+                }
+            }) => {
+                this.setData({product_list, total_page_count, total_product_count});
+            });
+    }
+});
