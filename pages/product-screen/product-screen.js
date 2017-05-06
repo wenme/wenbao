b0895bce95e1f7805397b0c6fa5c3e59dad06372
Ted Wu <iswujixiang@gmail.com>
2017-05-07 00:34:46 +0800

    feat: 产品筛选页

--- pages/product-screen/product-screen.js
+++ pages/product-screen/product-screen.js
@@ -1,4 +1,7 @@
 // pages/product-screen/product-screen.js
+const request           = require('../../utils/request');
+const {stringify}       = require('../../utils/qs');
+
 Page({
     
     data:{
@@ -27,11 +30,34 @@ Page({
             selected2:true
         })
     },
-    chooseD: function (event) {
-        if (this.class == 'chose') {
-            this.class = '';
-        } else {
-            this.class = 'chose';
-        }
+    select({target}) {
+        this.setData(target.dataset);
+    },
+    toList() {
+        let {keyword, insurerid, 'class': _class, structure}    = this.data;
+        let data            = {
+            product_class   : _class,
+            keyword,
+            insurer_id      : insurerid,
+            product_structure: structure
+        };
+        wx.redirectTo({
+            url: `../product-list/product-list?${stringify(data)}`
+        });
+    },
+    onLoad:function(data){
+        data.insurerid  = data.insurer_id;
+        delete data.insurer_id;
+        data.class      = data.product_class;
+        delete data.product_class;
+        data.structure  = data.product_structure;
+        delete data.product_structure;
+        this.setData(data);
+        request.withSessionKey({
+            url: 'https://wenme.cc/terms/get_search_filter'
+        })
+            .then(({data}) => {
+                this.setData(data);
+            });
     }
-})
+});
--- utils/qs.js
+++ utils/qs.js
@@ -3,4 +3,6 @@
  */
 
 module.exports.stringify    = (obj) => Object.keys(obj)
-    .map(key => `${key}=${obj[key]}`).join('&');
+    .filter(key => typeof obj[key] !== 'undefined')
+    .map(key => `${key}=${obj[key]}`)
+    .join('&');
