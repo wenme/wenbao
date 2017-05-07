ed1505807c4c7dcd353a4d7e691480ae278d265b
Ted Wu <iswujixiang@gmail.com>
2017-05-07 21:57:15 +0800

    feat: 热点问题列表（上拉加载）

--- pages/question-class-page/question-class-page.js
+++ pages/question-class-page/question-class-page.js
@@ -4,6 +4,20 @@ const request           = require('../../utils/request');
 Page({
     data:{},
 
+    more() {
+        let {data}  = this;
+        let {question_list}  = data;
+        data.page_num++;
+        request.withSessionKey({
+            url: `https://wenme.cc/helpdesk/get_question_list`,
+            data
+        })
+            .then(({data}) => {
+                data.question_list   = question_list.concat(data.question_list);
+                this.setData(data);
+            });
+    },
+
     onLoad(data){
         this.setData(data);
         request.withSessionKey({
