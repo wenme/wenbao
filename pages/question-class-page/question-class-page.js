318d557e90107ea6272a1457332bc50e5c423823
Ted Wu <iswujixiang@gmail.com>
2017-05-10 00:02:38 +0800

    fix: 热点问题列表下拉加载

--- pages/question-class-page/question-class-page.js
+++ pages/question-class-page/question-class-page.js
@@ -1,18 +1,31 @@
 // pages/question-class-page/question-class-page.js
 const request           = require('../../utils/request');
 
+let loading;
+
 Page({
     data:{},
 
     more() {
+        if (loading) {
+            return;
+        } else {
+            loading = true;
+        }
         let {data}  = this;
         let {question_list}  = data;
-        data.page_num++;
+        let page_num= data.page_num++;
         request.withSessionKey({
             url: `https://wenme.cc/helpdesk/get_question_list`,
             data
         })
             .then(({data}) => {
+                loading = false;
+                if (!(data.question_list && data.question_list.length)) {
+                    return this.setData({
+                        page_num
+                    });
+                }
                 data.question_list   = question_list.concat(data.question_list);
                 this.setData(data);
             });
